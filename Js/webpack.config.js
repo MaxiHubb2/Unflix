a241c6312d0f8c0dddcc53d9aac8d90c0d39f7e9
Maxi <mrabenko@alumno.unlam.edu.ar>
2024-08-12 20:07:12 -0300

    cambios con dotenv

--- Js/webpack.config.js
+++ Js/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: './src/admin.js', 
+  entry: './src/admin.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
