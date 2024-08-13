c1c653d1a366e6df99342d8eccdcf6095dd48d12
Maxi <mrabenko@alumno.unlam.edu.ar>
2024-08-13 00:30:17 -0300

    vuelve el config

---
+++ Js/config.js
@@ -0,0 +1,6 @@
+// config.js
+const config = {
+    SUPABASE_URL: 'https://rgiongzmlknshhkijite.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnaW9uZ3ptbGtuc2hoa2lqaXRlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMzIzOTMxMiwiZXhwIjoyMDM4ODE1MzEyfQ.QOrpVJyUJ0yO3JfBZnBUe_ZlFtjBzRSvEB8Ikgl6tgU'
+  };
+  
